f72fee8e15a3c06a37269cb6e92a88610d18b92c
Kea-2 <135198177+Kea-2@users.noreply.github.com>
2023-10-08 11:00:01 +1100

    quick push controller almost done i promse
    e

--- Fall GameJam/Assets/Scripts/CharacterStateMachine/CharacterGlideState.cs
+++ Fall GameJam/Assets/Scripts/CharacterStateMachine/CharacterGlideState.cs
@@ -1,25 +1,56 @@
 using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CharacterGlideState : CharacterBaseState
 {
     public CharacterGlideState(CharacterStateMachine stateMachine) : base(stateMachine) { }
 
+    private float tempYPos;
+    private float tempGrav;
+
     public override void EnterState()
     {
-        
+        tempYPos = stateMachine.transform.position.y;
+        tempGrav = stateMachine.gameObject.GetComponent<CustomGravity>().gravityStrength;
+
+        stateMachine.gameObject.GetComponent<CustomGravity>().gravityStrength = 1;
     }
 
     public override void UpdateState()
     {
-        if (!Input.GetKey(stateMachine.jumpKey))
+        stateMachine.HandleMoving(stateMachine.glideSpeed);
+
+        stateMachine.transform.position = new Vector3(stateMachine.transform.position.x, Mathf.Clamp(stateMachine.transform.position.y, Mathf.NegativeInfinity, tempYPos), stateMachine.transform.position.z);
+
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (!Input.GetKey(stateMachine.jumpKey) || stateMachine.currentGlideDuration >= stateMachine.maxGlideDuration)
         {
-            //change state to idle if horizontal and vertical input is 0 or set to walking/running if it its above zero (check if holding shift for runningd)
+            stateMachine.currentGlideDuration = 0;
+
+            if (verticalInput == 0 && horizontalInput == 0)
+            {
+                stateMachine.ChangeState(new CharacterIdleState(stateMachine));
+            }
+
+            if (Input.GetKey(stateMachine.sprintKey) && (verticalInput != 0 || horizontalInput != 0))
+            {
+                stateMachine.ChangeState(new CharacterWalkState(stateMachine));
+            }
+
+            if (verticalInput != 0 || horizontalInput != 0)
+            {
+                stateMachine.ChangeState(new CharacterWalkState(stateMachine));
+            }
         }
+
+        stateMachine.currentGlideDuration += Time.deltaTime;
     }
 
     public override void ExitState()
     {
-        
+        stateMachine.gameObject.GetComponent<CustomGravity>().gravityStrength = tempGrav;
     }
 }
